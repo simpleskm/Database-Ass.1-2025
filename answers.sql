c0515bcab434de98c49a6e7596cebc7564685b2f
simpleskm <simonmasila004@gmail.com>
2025-09-04 22:23:47 +0300

    answers.sql

---
+++ answers.sql
@@ -0,0 +1,14 @@
+-- Assignment: Introduction to Databases
+-- Smartstar Digital
+
+-- 1) Create a new database called salesDB
+CREATE DATABASE salesDB;
+
+-- 2) Drop (delete) the database called demo
+DROP DATABASE demo;
+
+-- 3) Show all databases (to verify creation/deletion)
+SHOW DATABASES;
+
+-- 4) Select (use) salesDB as the current working database
+USE salesDB;
